070b157d3d63351018fb4b5e6a52c15ca68b7da7
paulbyszkin <p.byszkin@gmail.com>
2021-10-27 11:13:41 +0200

    Migrated from Java 16 back to Java 8. - IntelliJ automatically expanded Record to Class. - Needed to migrate var to concrete type as type inference is not available in Java 8.

--- src/main/java/interview/santander/entities/AdjustedMarketData.java
+++ src/main/java/interview/santander/entities/AdjustedMarketData.java
@@ -1,5 +1,52 @@
 package interview.santander.entities;
 
-public record AdjustedMarketData(RawMarketData rawMarketData, double adjustedBid,
-                                 double adjustedAsk) {
+import java.util.Objects;
+
+public final class AdjustedMarketData {
+    private final RawMarketData rawMarketData;
+    private final double adjustedBid;
+    private final double adjustedAsk;
+
+    public AdjustedMarketData(RawMarketData rawMarketData, double adjustedBid,
+                              double adjustedAsk) {
+        this.rawMarketData = rawMarketData;
+        this.adjustedBid = adjustedBid;
+        this.adjustedAsk = adjustedAsk;
+    }
+
+    public RawMarketData rawMarketData() {
+        return rawMarketData;
+    }
+
+    public double adjustedBid() {
+        return adjustedBid;
+    }
+
+    public double adjustedAsk() {
+        return adjustedAsk;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        AdjustedMarketData that = (AdjustedMarketData) obj;
+        return Objects.equals(this.rawMarketData, that.rawMarketData) &&
+                Double.doubleToLongBits(this.adjustedBid) == Double.doubleToLongBits(that.adjustedBid) &&
+                Double.doubleToLongBits(this.adjustedAsk) == Double.doubleToLongBits(that.adjustedAsk);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rawMarketData, adjustedBid, adjustedAsk);
+    }
+
+    @Override
+    public String toString() {
+        return "AdjustedMarketData[" +
+                "rawMarketData=" + rawMarketData + ", " +
+                "adjustedBid=" + adjustedBid + ", " +
+                "adjustedAsk=" + adjustedAsk + ']';
+    }
+
 }
--- src/main/java/interview/santander/entities/RawMarketData.java
+++ src/main/java/interview/santander/entities/RawMarketData.java
@@ -1,6 +1,61 @@
 package interview.santander.entities;
 
-public record RawMarketData(String id, String instrumentName, double bid, double ask,
-                            long timestamp){
+import java.util.Objects;
+
+public final class RawMarketData {
+    private final String id;
+    private final String instrumentName;
+    private final double bid;
+    private final double ask;
+    private final long timestamp;
+
+    public RawMarketData(String id, String instrumentName, double bid, double ask,
+                         long timestamp) {
+        this.id = id;
+        this.instrumentName = instrumentName;
+        this.bid = bid;
+        this.ask = ask;
+        this.timestamp = timestamp;
+    }
+
+    public String instrumentName() {
+        return instrumentName;
+    }
+
+    public double bid() {
+        return bid;
+    }
+
+    public double ask() {
+        return ask;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        RawMarketData that = (RawMarketData) obj;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.instrumentName, that.instrumentName) &&
+                Double.doubleToLongBits(this.bid) == Double.doubleToLongBits(that.bid) &&
+                Double.doubleToLongBits(this.ask) == Double.doubleToLongBits(that.ask) &&
+                this.timestamp == that.timestamp;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, instrumentName, bid, ask, timestamp);
+    }
+
+    @Override
+    public String toString() {
+        return "RawMarketData[" +
+                "id=" + id + ", " +
+                "instrumentName=" + instrumentName + ", " +
+                "bid=" + bid + ", " +
+                "ask=" + ask + ", " +
+                "timestamp=" + timestamp + ']';
+    }
+
 
 }
